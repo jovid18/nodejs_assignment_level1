34978bbda7d81b5008a90ba85edc0762917d2ce0
jovid_18 <cshcho99@gmail.com>
2024-02-23 16:49:16 +0900

    Add error handling middleware

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import express from "express";
-import connect from "./schemas/index.js";
-import productRouter from "./routes/products.router.js";
-
+import express from 'express';
+import connect from './schemas/index.js';
+import productRouter from './routes/products.router.js';
+import notFoundErrorHandler from './middlewares/notFoundError.middleware.js';
+import generalErrorHandler from './middlewares/generalError.middleware.js';
 const app = express();
 const PORT = 3000;
 
@@ -12,26 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  return res.send("Hello World!");
+router.get('/', (req, res) => {
+  return res.send('Hello World!');
 });
 
-app.use("/api", productRouter);
+app.use('/api', productRouter);
 
-app.use((req, res, next) => {
-  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
-});
-
-app.use((err, req, res, next) => {
-  return res.status(err.status || 500).json({
-    error: {
-      message: err.message,
-    },
-  });
-});
+app.use(notFoundErrorHandler);
+app.use(generalErrorHandler);
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸어요!");
+  console.log(PORT, '포트로 서버가 열렸어요!');
 });
